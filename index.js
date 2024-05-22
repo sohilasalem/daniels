bc0cad86bb68d1a352c591765de6001d1de04363
sohilasalem1123@gmail.com <sohilasalem1123@gmail.com>
2024-05-22 12:00:46 +0300

    First commet

---
+++ index.js
@@ -0,0 +1,20 @@
+// var  freinds=['ali','omar','ahmed','adham','osama','amr','abdo'];
+// function findName(term)
+// {
+// var result=[];
+// for(var i=0;i<freinds;i++)
+//     {
+// if(freinds[i]==trem)
+//     {
+//        result.push(i)
+//     }
+// }
+// return result
+// }
+
+// var x=findName=('omar');
+// console.log(x);
+
+// const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+//     target: '#navbar-example'
+//   })
